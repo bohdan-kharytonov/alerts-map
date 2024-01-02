4f97a4d9ba17564d2820c55519932277ffb4f349
bohdan-kharytonov <bohdan.kharytonov@lembergsolutions.com>
2024-01-02 16:28:53 +0200

    add the ability to receive current alarms in areas by id

---
+++ alerts.py
@@ -0,0 +1,23 @@
+# This Python file uses the following encoding: utf-8
+
+import urllib.request
+import json
+
+class alerts:
+    def __init__(self):
+        pass
+    def getActualData(self):
+        contents = urllib.request.urlopen("https://api.alerts.in.ua/v1/alerts/active.json?token=").read()
+        json_data_str = contents.decode("utf-8") if isinstance(contents, bytes) else contents
+        data = json.loads(json_data_str)
+
+        location_oblast_uids = [alert["location_oblast_uid"] for alert in data["alerts"]]
+
+        print(location_oblast_uids)
+
+
+
+
+
+
+
