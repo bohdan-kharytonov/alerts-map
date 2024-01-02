1fa68730b4c0f6210ad251c6840a5ffac9076a79
bohdan-kharytonov <bohdan.kharytonov@lembergsolutions.com>
2024-01-02 19:13:00 +0200

    add simple hide and show for qlabel

--- widget.py
+++ widget.py
@@ -1,8 +1,8 @@
 # This Python file uses the following encoding: utf-8
 import sys
 
-from PySide6.QtWidgets import QApplication, QWidget
-
+from PySide6.QtWidgets import QApplication, QWidget, QLabel
+from PySide6.QtGui import QPixmap, QColor, QPalette
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
@@ -17,11 +17,33 @@ class Widget(QWidget):
         self.ui = Ui_Widget()
         self.ui.setupUi(self)
 
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = Widget()
     a = alerts()
-    a.getActualData()
+    data = a.getActualData()
+    if '19' in str(data):
+        label_poltava = widget.findChild(QLabel, "poltava")
+        label_poltava.show()
+        pm = label_poltava.pixmap()
+        pm.fill(QColor('#FFFFFF'))
+        label_poltava.setPixmap(pm)
+        label_poltava.setMask(pm.mask())
+        label_poltava.hide()
+        label_poltava.show()
+    else:
+        label_poltava = widget.findChild(QLabel, "poltava")
+        label_poltava.hide()
+        # hui = pixmap->fromFile
+        # label_poltava.setPixma
+        pm = label_poltava.pixmap()
+        pm.fill(QColor('#FFFFFF'))
+        label_poltava.setPixmap(pm)
+        label_poltava.setMask(pm.mask())
+        label_poltava.hide()
+
+    label_sumy = widget.findChild(QLabel, "sumy")
+    label_sumy.hide()
+
     widget.show()
     sys.exit(app.exec())
